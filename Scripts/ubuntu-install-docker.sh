c19e202cde595735f7d72856be0f9521e55812be
Luxior <296337@supinfo.com>
2020-04-28 19:25:19 +0200

    fix errors only on linux

--- Scripts/ubuntu-install-docker.sh
+++ Scripts/ubuntu-install-docker.sh
@@ -1,12 +1,13 @@
 #!bin/bash
 apt-get update && apt-get upgrade && apt-get dist-upgrade -y| # Update ubuntu distribution
 
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common nano python auditd -y | # Installing docker and other packages
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common nano python auditd default-jdk node npm -y | # Installing docker and other packages
 
-docker run hello-world
-docker-compose --version
-
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get install jenkins
+sudo npm install pm2 -g
+sudo ln -s /usr/bin/nodejs /usr/local/bin/node
+sudo ln -s /usr/bin/npm /usr/local/bin/npm
+sudo apt-get install build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
